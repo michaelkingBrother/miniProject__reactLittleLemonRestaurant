0f9421eccaeb449ebef9c52cd50bf6766ef7f78c
HỒ PHƯỚC TRUNG <ht.hophuoctrung@gmail.com>
2024-05-12 23:40:52 +0700

    chores: Setting up a semantic HTML document, add meta tag and setup OGP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Little Lemon web site HomePage
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
