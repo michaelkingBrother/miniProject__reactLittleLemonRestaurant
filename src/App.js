c14bcd3e14f9827c9af6c83c66aaa42ddebd83d6
HỒ PHƯỚC TRUNG <ht.hophuoctrung@gmail.com>
2024-05-17 00:19:52 +0700

    chores: setup router for navigation to another page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
+import { Routes, Route, Router } from 'react-router-dom';
 
 function App() {
   return (
